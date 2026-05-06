13464cabff67bffd41df3922f5f1cea92d33951c
Saheli-M01 <sahelimondal7845@gmail.com>
2026-05-06 11:50:14 +0530

    add:Grid.tsx with static data

--- app/src/components/GridGame.tsx
+++ app/src/components/GridGame.tsx
@@ -1,5 +1,20 @@
+"use client";
 import Header from "./Header";
+import Grid from "./Grid";
 export default function GridGame() {
+  //for testing
+  const cols = 40;
+  const rows = 30;
+
+  const grid = Array.from({ length: cols * rows }, (_, i) => ({
+    owner: i % 5 === 0 ? "user1" : null,
+    color: i % 5 === 0 ? "#3B82F6" : null,
+    name: i % 5 === 0 ? "Diya" : null,
+    claimedAt: Date.now(),
+    lockedUntil: null,
+    contestCount: Math.floor(Math.random() * 10),
+  }));
+
   return (
     <>
       {" "}
@@ -8,6 +23,23 @@ export default function GridGame() {
         style={{ backgroundColor: "var(--color-bg)" }}
       >
         <Header />
+        <div className="flex flex-1 overflow-hidden">
+          <main className="flex-1 flex items-center justify-center overflow-hidden p-4">
+            {" "}
+            <Grid
+              grid={grid}
+              cols={cols}
+              rows={rows}
+              userId="user1"
+              myColor="#3B82F6"
+              cooldownUntil={0}
+              myBombs={2}
+              lastBombIndices={[]}
+              onCellClick={() => {}}
+              onCellHover={() => {}}
+            />
+          </main>
+        </div>
       </div>
     </>
   );
